9d41abd1e74eea1e1d1ea79b32f78ce873973661
Phanthat Muenprap <phanthat54@gmail.com>
2022-01-11 17:43:14 +0700

    lab_15 failed hidden test

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	for (int i=1; i<N; i++){
+        int j = i, tmp = d[i];
+        while (j > 0 && d[j-1] < d[j]){
+            tmp = d[j];
+            d[j] = d[j-1];
+            d[j-1] = tmp;
+            j--;
+        }
 
+        cout <<"Pass "<<i<<":";
+        for (int i = 0; i < 10; i++) cout << d[i] << " ";
+        cout<< "\n";
+    }
 }
 
 int main(){
